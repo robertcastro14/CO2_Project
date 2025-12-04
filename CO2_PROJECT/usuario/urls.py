c2712dbe6babe9e7156eb0e8f914e4bf06603978
Thiago Duarte <thiagoduarte.cei@hotmail.com>
2025-12-04 20:04:57 +0000

    Codigo Atualizado

--- CO2_PROJECT/usuario/urls.py
+++ CO2_PROJECT/usuario/urls.py
@@ -4,6 +4,8 @@ from usuario import views  # Importando as views que você acabou de criar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),               # Rota da Página Inicial
-    path('calculadora/', views.calculadora, name='calculadora'), # Rota da Calculadora
+    path('', views.home, name='home'), # Rota da Página Inicial
+    path('calculadora/', views.calculadora, name='calculadora'),
+    path('usuario/', include('usuario.urls')), # Rota da Calculadora
+    path('', include('usuario.urls')),
 ]
